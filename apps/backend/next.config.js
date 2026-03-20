bc6e214d45235883bfeee5295ed8df2fb41e2a1e
mahmod al-sharaky <93828510+mshraky3@users.noreply.github.com>
2026-03-20 05:20:44 +0300

    Add files via upload

--- apps/backend/next.config.js
+++ apps/backend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ["@wedgite/types"],
+    outputFileTracingIncludes: {
+        "/api/screenshot": ["./node_modules/@sparticuz/chromium/bin/**"],
+    },
 };
 
 module.exports = nextConfig;
